12c8ab48f7b75512f2a85b2a08e5f9714bbdabb5
Andrés Pérez Merello <andresperezmerello@MacBook-Pro-de-Andres.local>
2023-07-26 18:18:41 -0300

    ultimos ajustes

--- forms/contact.php
+++ forms/contact.php
@@ -5,10 +5,9 @@
   * The library should be uploaded to: vendor/php-email-form/php-email-form.php
   * For more info and help: https://bootstrapmade.com/php-email-form/
   */
-  require '../assets/vendor/php-email-form/php-email-form.php';
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'andresperez_98@hotmail.com, andresperezmerello@gmail.com';
+  $receiving_email_address = 'andresperez_98@hotmail.com';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
